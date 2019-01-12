8a4ff1d7efe9580958b88ed499f239afe98a1e29
miun173 <fahmifan173@gmail.com>
2019-01-12 15:11:37 +0700

    create QuoteItem

---
+++ src/container/Quotes/QuoteItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="quote uk-card uk-card-default uk-card-body uk-card-hover"
+    @mouseover="isShow = true" @mouseout="isShow = false">
+    <p class="uk-card-title">{{ quote }}</p>
+    <span uk-icon="trash" class="uk-margin-small-right icon"
+      v-show="isShow" 
+      @click="deleteQuote()"></span>
+  </div>
+</template>
+
+<script>
+import { eventBus } from "../../main.js" 
+export default {
+  props: ['quote', 'id'],
+  data() {
+    return {
+      isShow: false,
+    }
+  },
+  methods: {
+    deleteQuote() {
+      eventBus.deleteQuote(this.id)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.quote {
+  cursor: pointer;
+  &:hover {
+    background-color: #1E87F0;
+    p {
+      color: #fff;
+    }
+
+    .icon {
+      fill: #fff;
+      color: #fff;
+    }
+  }
+}
+</style>
