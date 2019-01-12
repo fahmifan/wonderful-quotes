96929d88493c6e9b1a9377b2ecb0ee3cd4be664b
miun173 <fahmifan173@gmail.com>
2019-01-12 15:10:31 +0700

    add eventBus

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+export const eventBus = new Vue({
+  methods: {
+    deleteQuote(quoteId) {
+      this.$emit('quoteDeleted', quoteId)
+    }
+  }
+})
+
 new Vue({
   el: '#app',
   render: h => h(App)
